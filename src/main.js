8a5c27fa454aa608b3f49a12a7870ee8f152d11f
Dasha Timeeva <d.timeeva@yandex.ru>
2026-06-07 20:07:08 +0500

    исправление options

--- src/main.js
+++ src/main.js
@@ -132,12 +132,12 @@ function analyzeSalesData(data, options) {
     });
 
     return sellerStats.map((seller) => ({
-        bonus: +seller.bonus.toFixed(2),
-        name: seller.name,
-        profit: +seller.profit.toFixed(2),
-        revenue: +seller.revenue.toFixed(2),
-        sales_count: seller.sales_count,
         seller_id: seller.id,
-        top_products: seller.top_products
+        name: seller.name,
+        revenue: +seller.revenue.toFixed(2),
+        profit: +seller.profit.toFixed(2),
+        sales_count: seller.sales_count,
+        top_products: seller.top_products,
+        bonus: +seller.bonus.toFixed(2)
     }));
 }
